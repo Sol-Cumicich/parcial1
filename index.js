38365c797e8f1d92e9e49125ead839725d5b35d5
Sol-Cumicich <maria.cumicich@davinci.edu.ar>
2024-11-18 21:39:59 -0300

    primer commit

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 import express from "express";
 
 const app = express();
+const port = 3000;
+app.use(express.json())
 
-//middleware - el servidor interpreta mejor los datos complejos
-app.use(express.urlencoded({extended: true}))
-
-// app.post("/")
-// app.put("/")
 // app.delete("/")
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send("Hola con express!")
 })
 
-app.get("/params/:id", (req, res) => {
+app.get('/params/:id', (req, res) => {
     let id = req.params.id;
     res.send(`Tu id es: ${id}`)
 })
 
-const usuarios = [
-    {id: 1, nombre: "Sol", apellido: "Cumicich", edad: 34, userType: "Escritor"},
-    {id: 2, nombre: "Evelyn", apellido: "Oliveira", edad: 32, userType: "Lector"},
-    {id: 2, nombre: "Alex", apellido: "Rivero", edad: 26, userType: "Escritor"},
-    {id: 3, nombre: "Solange", apellido: "Oliveira", edad: 32, userType: "Lector"}
+const users = [
+    {id: 1, name: "Sol", lastname: "Cumicich", age: 34, userType: "Escritor", email: "sol@gmail.com"},
+    {id: 2, name: "Evelyn", lastname: "Oliveira", age: 32, userType: "Lector", email: "eve@gmail.com"},
+    {id: 3, name: "Alex", lastname: "Rivero", age: 26, userType: "Escritor", email: "alex@gmail.com"},
+    {id: 4, name: "Solange", lastname: "Oliveira", age: 32, userType: "Lector", email: "solange@gmail.com"}
 ]
 
 const fanfics = [
@@ -33,26 +30,71 @@ const fanfics = [
     {id: 3, foto: "", titulo: "Fanfic3", descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", autor: "userName1"}
 ]
 
-app.get("/users", (req, res) => {
-    res.send(usuarios)
+/*-------------------------------------USERS------------------------------*/
+
+app.get('/users', (req, res) => {
+    res.status(200).json(users)
 })
 
-app.get("/users/userType", (req, res) => {
+app.get('/users/userType', (req, res) => {
     let userType = req.query.userType;
     if(!userType || (userType !== "Escritor" && userType !== "Lector")) return res.send({error: "Tipo de usuario no valido"});
-    let filtrado = usuarios.filter(u => u.userType === userType);
-    res.send({usuarios: filtrado})
+    let filtrado = users.filter(u => u.userType === userType);
+    res.send({users: filtrado})
 })
 
-app.get("/fanfics", (req, res) => {
+app.get('/users/filterbyid/:id', (req, res) => {
+    let userId = parseInt(req.params.id);
+    let user = users.find(u => u.id === userId);
+
+    if(user){
+        res.status(200).json(user);
+    }else{
+        res.status(404).json({message: "Usuario no encontrado!", code: 3312})
+    }
+})
+
+//Crear Usuario
+app.post('/users', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        lastname: req.body.lastname,
+        age: req.body.age,
+        userType: req.body.userType,
+        email: req.body.email
+    }
+
+    users.push(newUser);
+    res.status(201).json(newUser)
+});
+
+//Actualizar Usuario
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const userIndex = users.findIndex(u => u.id === userId);
+
+    if(userIndex !== -1){
+        users[userIndex] = {id: userId, ...req.body};
+    }
+});
+
+
+/*---------------------FANFIC-------------------------*/
+
+app.get('/fanfics', (req, res) => {
     res.send(fanfics)
 })
 
-app.get("/fanfics/filterbyid/:id", (req, res) => {
-    let fanficId = req.params.id;
-    let fanfic = fanfics.find(f => f.id === parseInt(fanficId));
-    if(!fanfic) return res.send({error: "3312: Fanfic no encontrado!"});
-    res.send({fanfic})
+app.get('/fanfics/filterbyid/:id', (req, res) => {
+    let fanficId = parseInt(req.params.id);
+    let fanfic = fanfics.find(f => f.id === fanficId);
+
+    if(fanfic){
+        res.status(200).json(fanfic);
+    }else{
+        res.status(404).json({message: "Fanfic no encontrado!", code: 3312})
+    }
 })
 
 
@@ -61,4 +103,6 @@ app.get("/fanfics/filterbyid/:id", (req, res) => {
 //
 
 
-app.listen(3000, () => console.log("server running on port... \n open in https://localhost:3000"))
+app.listen(port, () => {
+    console.log(`server running on port... \n open in https://localhost:${port}`);
+})
